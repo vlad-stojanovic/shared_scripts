f76a5246ce0afa4a0bb47c7bfbdae8e75985b3f8
Vladimir Stojanović <vstojanovic@microsoft.com>
2021-08-16 15:40:56 +0200

    Clean deleted branches script

---
+++ git/clean_deleted_branches.ps1
@@ -0,0 +1,26 @@
+# Include git helper functions
+. "$($PSScriptRoot)/_git_common.ps1"
+
+# Update list of branches from remote origin
+UpdateBranchesInfoFromRemote
+
+# List all branches & filter only the branches deleted from the remote origin
+$deletedBranchRegex = "((: )|(\[))gone\]"
+[string[]]$currentBranchesDeleted = git branch --verbose | Where-Object { $_ -imatch "\*.*$($deletedBranchRegex)" } | ForEach-Object { $_.Trim().Split()[1] }
+If ($currentBranchesDeleted.Count -Gt 0) {
+	# Switch to master branch to enable deletion of the current branch
+	# which is deleted from the remote origin
+	LogWarning "Switching away from current branch [$($currentBranchesDeleted[0])] which is deleted from the remote origin"
+	RunGitCommandSafely "git checkout master"
+}
+
+[string[]]$allDeletedBranches = git branch --verbose | Where-Object { $_ -imatch $deletedBranchRegex } | ForEach-Object { $_.Trim().Split()[0] }
+If ($allDeletedBranches.Count -Gt 0) {
+	LogWarning "Cleaning $($allDeletedBranches.Count) deleted remote branche(s)"
+	# Clean the remotely deleted branches, force to avoid asking for confirmation
+	ForEach ($deletedBranch in $allDeletedBranches) {
+		git branch -df $deletedBranch
+	}
+} Else {
+	LogSuccess "No deleted remote branches to clean locally"
+}
